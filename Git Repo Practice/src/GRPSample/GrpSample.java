42162a72f02b8bbff97e5e667ae2a888cc97251e
MAQ <MAQ@DESKTOP-5D3619Q>
2019-07-29 20:36:51 +0530

    Sample Repo For Practice

---
+++ Git Repo Practice/src/GRPSample/GrpSample.java
@@ -0,0 +1,11 @@
+package GRPSample;
+
+public class GrpSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----Welcome To GIT Repository------");
+
+	}
+
+}
